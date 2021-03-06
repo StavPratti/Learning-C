06d2c1b820e73fa7bc31e72e091e6c25b940345d
Stavroula_Pratti <56409214+StavPratti@users.noreply.github.com>
2021-03-07 01:34:29 +0200

    Update strlen.c

--- strlen.c
+++ strlen.c
@@ -6,9 +6,10 @@ int main()
     char a[20]="Program";
     char b[20]={'P','r','o','g','r','a','m','\0'};
 
-    // using the %zu format specifier to print size_t
+    // using the %zu format specifier to print size_t or %d
     printf("Length of string a = %zu \n",strlen(a));
     printf("Length of string b = %zu \n",strlen(b));
-
+    // null character doesnt count
+    
     return 0;
 }
