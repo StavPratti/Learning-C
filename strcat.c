cf019661f1945f36f65c58adf4a52dfda55d51df
Stavroula_Pratti <56409214+StavPratti@users.noreply.github.com>
2021-03-07 12:14:22 +0200

    Add files via upload

---
+++ strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   char str1[100] = "This is ", str2[] = "programiz.com";
+
+   // concatenates str1 and str2
+   // the resultant string is stored in str1
+   //str1 is destination string while str2 is source string
+   strcat(str1, str2);
+
+   puts(str1);
+   puts(str2);
+
+   return 0;
+}
