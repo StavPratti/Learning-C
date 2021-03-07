b03bf9fb87c5d5f31b4858bbeaf07591d50f98ed
Stavroula_Pratti <56409214+StavPratti@users.noreply.github.com>
2021-03-07 21:58:32 +0200

    Add files via upload

---
+++ hanoi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hanoi(int N, int start, int target, int temp) {
+
+	if (N == 1) {
+		printf("%d -> %d\n", start, target);
+		return;
+	}
+
+	hanoi(N-1, start, temp, target);
+	hanoi(1, start, target, temp); //temp isnt used
+	hanoi(N - 1, temp, target, start);
+
+}
+
+int main() {
+
+	hanoi(4, 1, 3, 2);
+
+	return 0;
+}
